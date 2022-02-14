4491bdd17baa19b369a2d316afb081c703d9375a
Rory Potter <29921813+RoryPotter@users.noreply.github.com>
2022-02-14 22:04:12 +0000

    Create session

---
+++ app/utils/session.server.ts
@@ -0,0 +1,53 @@
+import bcrypt from "bcryptjs";
+import { createCookieSessionStorage, redirect } from "remix";
+import { db } from "./db.server";
+
+type LoginType = {
+  username: string;
+  password: string;
+};
+
+export async function login({ username, password }: LoginType) {
+  let existingUser = await db.user.findFirst({ where: { username } });
+  if (!existingUser) return null;
+
+  const passwordsMatch = await bcrypt.compare(
+    password,
+    existingUser.passwordHash
+  );
+
+  if (!passwordsMatch) return null;
+
+  return existingUser;
+}
+
+const sessionSecret = process.env.SESSION_SECRET;
+if (!sessionSecret) {
+  throw new Error("SESSION_SECRET must be set");
+}
+
+const storage = createCookieSessionStorage({
+  cookie: {
+    name: "RJ_session",
+    // normally you want this to be `secure: true`
+    // but that doesn't work on localhost for Safari
+    // https://web.dev/when-to-use-local-https/
+    secure: process.env.NODE_ENV === "production",
+    secrets: [sessionSecret],
+    sameSite: "lax",
+    path: "/",
+    maxAge: 60 * 60 * 24 * 30, // 30 days
+    httpOnly: true,
+  },
+});
+
+export async function createUserSession(userId: string, redirectTo: string) {
+  const session = await storage.getSession();
+  session.set("userId", userId);
+
+  return redirect(redirectTo, {
+    headers: {
+      "Set-Cookie": await storage.commitSession(session),
+    },
+  });
+}
